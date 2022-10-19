fdeb12a6dac1c642396b9a8530d47c01656b8c28
jhnlambert <jhnlambert171@gmail.com>
2022-10-18 20:06:07 -0500

    Initial MiniBot code creation
    code went through 2022 WPI code refresh.
    
    Started setting up minibot code

--- src/main/java/org/team5148/training/Robot.java
+++ src/main/java/org/team5148/training/Robot.java
@@ -1,11 +1,13 @@
 package org.team5148.training;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkMax;
+//import com.revrobotics.SparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
 
 public class Robot extends TimedRobot {
 
@@ -13,6 +15,12 @@ public class Robot extends TimedRobot {
 	CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
 	CANSparkMax backLeft = new CANSparkMax(2, MotorType.kBrushless);
 	CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
+
+	//xbox controller
+	XboxController driveController = new XboxController(0);
+
+	//define a drive
+	MecanumDrive MDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 
 	@Override
 	public void autonomousInit() {
@@ -30,9 +38,12 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		frontLeft.set(-0.1);
-		frontRight.set(0.1);
-		backLeft.set(-0.1);
-		backRight.set(0.1);
+		//use controller input to set drive system
+		
+		float ySpeed = 0;
+		float xSpeed = 0;
+		float zRotation = 0; 
+		MDrive.driveCartesian(ySpeed, xSpeed, zRotation);
+		
 	}
 }
