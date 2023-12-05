23f30ebf84348c7485be55424f7d59f5b6131e85
David Schulz <deschulz22@gmail.com>
2023-12-04 18:42:48 -0600

    Update project year, restart project with unfinished tank example

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends TimedRobot {
+  private final TalonSRX frontLeft = new TalonSRX(1);
+  private final TalonSRX frontRight = new TalonSRX(2);
+  private final TalonSRX backLeft = new TalonSRX(3);
+  private final TalonSRX backRight = new TalonSRX(4);
+  private final Joystick leftJoystick = new Joystick(0);
+  private final Timer m_timer = new Timer();
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    // rightDrive.setInverted(true);
+  }
+
+  /** This function is run once each time the robot enters autonomous mode. */
+  @Override
+  public void autonomousInit() {
+    m_timer.restart();
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      // Drive forwards half speed, make sure to turn input squaring off
+      //m_robotDrive.arcadeDrive(0.5, 0.0, false);
+    } else {
+      //m_robotDrive.stopMotor(); // stop robot
+    }
+  }
+
+  /** This function is called once each time the robot enters teleoperated mode. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during teleoperated mode. */
+  @Override
+  public void teleopPeriodic() {
+    frontLeft.set(TalonSRXControlMode.PercentOutput, -leftJoystick.getY() * 0.2);
+    frontRight.set(TalonSRXControlMode.PercentOutput, leftJoystick.getRawAxis(3) * 0.2);
+    backLeft.set(TalonSRXControlMode.PercentOutput, -leftJoystick.getY() * 0.2);
+    backRight.set(TalonSRXControlMode.PercentOutput, leftJoystick.getRawAxis(3) * 0.2);
+  }
+
+  /** This function is called once each time the robot enters test mode. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
